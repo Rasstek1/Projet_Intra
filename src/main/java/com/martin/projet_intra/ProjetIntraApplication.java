60d7cd66b7cd7ec0cdea7dea4477bc50481be645
Martin Racine <2295093@cegepoutaouais.qc.ca>
2023-10-05 06:28:55 -0400

    first push

---
+++ src/main/java/com/martin/projet_intra/ProjetIntraApplication.java
@@ -0,0 +1,13 @@
+package com.martin.projet_intra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIntraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetIntraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martin/projet_intra/ProjetIntraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.projet_intra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIntraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
